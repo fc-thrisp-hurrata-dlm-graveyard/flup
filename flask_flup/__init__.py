45efd72560357f177209aee4025918919600e8c1
blueblank <blueblank@gmail.com>
2013-04-25 16:57:50 -0400

    make Uploadset, etc accessible

--- flask_flup/__init__.py
+++ flask_flup/__init__.py
@@ -1,3 +1,4 @@
 __version__ = "0.0.1"
 
-from flup import Flup
+from flup import (Flup, UploadSet, TEXT, DOCUMENTS, IMAGES, AUDIO, DATA,
+        SCRIPTS, ARCHIVES, EXECUTABLES, DEFAULTS, All, AllExcept)
